43540090b81d9b352f8c062931f457e7a46cf5a4
zhiluan <1733083957@qq.com>
2023-10-20 00:16:25 +0800

    n阶幻方矩阵.cpp

---
+++ n阶幻方矩阵.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+int main()
+{
+	int t,i,j,k,m=0,n,sum=0,item=0,flag=1;;
+	int a[100][100]={0};	
+	scanf("%d",&t);
+	for(k=1;k<=t;k++)
+	{ 
+		sum=0;
+		flag=1;
+		scanf("%d",&n);
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				scanf("%d",&a[i][j]);
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			sum=sum+a[i][i];
+		}
+		for(i=0;i<n;i++)
+		{	
+			item=0;				
+			for(j=0;j<n;j++)
+			{
+				item=item+a[i][j];
+			}
+			if(item!=sum)
+			{
+				flag=0;
+			}	
+		}		
+		for(j=0;j<n;j++)
+		{
+			item=0;	
+			for(i=0;i<n;i++)
+			{
+				item=item+a[i][j];
+			}			
+			if(item!=sum)
+			{
+				flag=0;
+			}
+		}
+		item=0;	
+		for(i=0;i<n;i++)
+		{
+			item=item+a[i][n-1-i];
+		}
+		if(item!=sum)
+		{
+			flag=0;
+		}				
+		if(flag==0)
+		{
+			printf("NO\n");
+		}
+		else
+		{
+			printf("YES\n");
+		}
+	}
+	return 0;
+}
